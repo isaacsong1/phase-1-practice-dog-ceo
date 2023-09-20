017c391268c0e6c9888c9c7fa9576465b8f96e54
Isaac Song <isaacsongs10@gmail.com>
2023-09-20 10:53:26 -0700

    Fetch dog lab complete

--- src/index.js
+++ src/index.js
@@ -1,51 +1,58 @@
 console.log('%c HI', 'color: firebrick');
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     fetch(imgUrl)
     .then(resp => resp.json())
-    .then(obj => obj.message.forEach(img => {
-        const image = document.createElement("img");
-        image.src = img;
-        image.alt = "Picture of a Dog"
-
-        document.getElementById("dog-image-container").append(image);
+    .then(obj => {
+        obj.message.forEach(img => addImage(img));
     })
-    )
     .catch(error => alert(error));
 
     fetch(breedUrl)
     .then(resp => {
         if (!resp.ok) {
-            alert("Faulty response");
+            alert('Faulty response');
         } 
         return resp.json();
     })
     .then(obj => { 
         breeds = Object.keys(obj.message);
-        // for (let breed in obj.message) {
-        //     const li = document.createElement("li");
-        //     li.textContent = breed;
-        //     li.addEventListener("click", () => {
-        //         li.style.color = "red";
-        //     })
-            
-        //     document.getElementById("dog-breeds").append(li);
-        // }
-        // const dropdown = document.getElementById("breed-dropdown");
-        // dropdown.addEventListener("click", (event, obj) => {
-
-        //     if (event.target.value !== "default") {
-
-        //     }
-        // })
+        renderBreedList(breeds);
+        dropdownListener();
     })
     .catch(error => alert(error));
-    //     .forEach(breed => {
-    //     debugger;
-
-    // })
-    // )
 })
+
+// Helper Functions
+function addImage(img) {
+    const image = document.createElement('img');
+    image.src = img;
+    image.alt = 'Picture of a Dog'
+
+    document.getElementById('dog-image-container').append(image);
+}
+
+function renderBreedList(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    let liChild = ul.lastElementChild;
+    while (!!liChild) {
+        ul.removeChild(liChild);
+        liChild = ul.lastElementChild;
+    }
+    breeds.forEach(breed => {
+        let li = document.createElement('li');
+        li.textContent = breed;
+        li.addEventListener('click', event => event.target.style.color = 'red');
+        ul.appendChild(li);
+    })
+}
+
+function dropdownListener() {
+    const dropdown = document.querySelector('#breed-dropdown');
+    dropdown.addEventListener('change', event => {
+        renderBreedList(breeds.filter(breed => breed.startsWith(event.target.value)));
+    })
+}
